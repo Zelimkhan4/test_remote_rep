7ec2e3e51eeb33d72701d9b7ecd0188fe3f8c5b2
Zelimkhan4 <vip.guchiev@mail.ru>
2022-12-08 14:39:16 +0300

    Add some.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     print("Hi Zelimkhan")
     print("Hi Yandex")
     print("i edit this project")
+    print("one, two, three, four")
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
